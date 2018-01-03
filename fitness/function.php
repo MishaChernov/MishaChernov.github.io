25f3532d4b0c61733dab3fda0913098d60de6775
Misha Chernov <mishachernov8063@gmail.com>
2018-01-03 23:27:50 +0200

    fitness v2

---
+++ fitness/function.php
@@ -0,0 +1,24 @@
+<?php
+
+if(isset($_POST['submit'])) {
+	$first_name = $_POST['first_name'];
+	$last_name = $_POST['last_name'];
+	$phone = $_POST['phone'];
+	$email = $_POST['email'];
+
+
+
+
+
+	$msg = 'שם : ' . $first_name ."\n"
+	.'טלפון: ' . $phone ."\n"
+	. 'דואר אלקטרוני: ' . $email."\n" ;
+
+
+	 mail('eyalivne@gmail.com' ,'נרשם חדש - KMT - מרכזיית VOIP' , $msg);
+
+	//mail($to,$subject,$txt,$headers);
+		header('Location: thankyou.php');
+		echo $msg;
+}
+?>
