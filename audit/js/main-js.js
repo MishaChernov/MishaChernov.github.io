c094be01f6e445512a66059cf4079c2e627d8a2a
Misha Chernov <mishachernov8063@gmail.com>
2018-02-19 21:09:14 +0200

    audit v1.1

--- audit/js/main-js.js
+++ audit/js/main-js.js
@@ -1,1 +1,18 @@
 'use strict'
+
+//hide all tabs first
+$('.mich_info__content').hide();
+//show the first tab content
+$('#mich_info__content-4').show();
+
+$('.mich_info__radio').change(function () {
+ var dropdown = $(this).val();
+  //first hide all tabs again when a new option is selected
+  $('.mich_info__content').hide();
+  //then show the tab content of whatever option value was selected
+  $('#' + "mich_info__content-" + dropdown).show();
+    //go to #id link
+  if(window.innerWidth <= 800) {
+    window.location.href = `#mich_info__content-${dropdown}`;
+  }
+});
