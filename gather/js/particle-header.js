4374a4de7fef1c98d8f556034e6e6c64e176dee8
Misha Chernov <mishachernov8063@gmail.com>
2018-08-01 19:32:56 +0300

    gather v1.0.4

--- gather/js/particle-header.js
+++ gather/js/particle-header.js
@@ -22,7 +22,7 @@ particlesJS("particles-js", {
             "image": {
                 "src": "img/github.svg",
                 "width": 100,
-                "height": 100
+                "height": 0
             }
         },
         "opacity": {
