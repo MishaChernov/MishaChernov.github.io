018c8f10113588f4d371add355aa206cc49585f4
Misha Chernov <80amulet63@gmail.com>
2018-08-23 01:21:52 +0300

    7.0.0

--- brokers/js/index.js
+++ brokers/js/index.js
@@ -2,6 +2,136 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
+
+	// Start Check The Window Height On Broker Profile page
+
+	if(document.getElementsByClassName('page-broker-profile').length > 0) {
+
+
+		let windowHeight = $(window).height() - 135;
+		let asideHeight = $('.profile-card').height();
+
+		if( (windowHeight - asideHeight) >= 20) {
+			document.getElementsByClassName('page-broker-profile')[0].classList.add('page-broker-profile--height');
+		}
+	}
+
+	// End Check The Window Height On Broker Profile page
+
+
+
+	// Start Find Profile Stickers
+
+	if(document.getElementsByClassName('profile').length > 0) {
+		var profile = document.getElementsByClassName('profile');
+
+		if(profile[0].classList.contains('profile--pro') == true) {
+			document.querySelector('.sticker--pro').style.display = 'block';
+		}
+	}
+
+	// End Find Profile Stickers
+
+
+	// Start Check Feedbacks If They Are
+
+	if(document.getElementsByClassName('broker-feedback__list').length > 0) {
+
+		let feedbackList = document.getElementsByClassName('broker-feedback__list');
+
+		if(!feedbackList[0].hasChildNodes()) {
+
+			let feedbackButton = document.getElementsByClassName('broker-feedback__no-feedbacks');
+			feedbackButton[0].classList.add('broker-feedback__no-feedbacks--visible');
+
+			let pageNav = document.getElementsByClassName('page-nav__list');
+			pageNav[0].classList.add('page-nav__list--hidden');
+		}
+	}
+
+	// End Check Feedbacks If They Are
+
+
+
+	// Start Check If There Is Broker Picture
+
+	if(document.getElementsByClassName('profile-card__image-wrapper').length > 0) {
+		if(!document.getElementsByClassName('profile-card__image')[0].getAttribute('src').length > 0) {
+
+			document.getElementsByClassName('profile-card__image')[0].style.display = 'none';
+
+
+			var colors = ['#382c75', '#0cb175', '#000000', '#18316b', '#f2744d', '#faa61a'];
+			let randomNum = Math.floor(Math.random() * 6);
+
+			document.getElementsByClassName('profile-card__image-wrapper')[0].style.background = (colors[randomNum]);
+
+
+			let profileNameFirst = document.getElementsByClassName('profile-card__name-first')[0].innerHTML.charAt(0);
+			let profileNameSecond = document.getElementsByClassName('profile-card__name-second')[0].innerHTML.charAt(0);
+
+			document.getElementsByClassName('profile-card__without-picture')[0].innerHTML = profileNameFirst + profileNameSecond;
+			document.getElementsByClassName('profile-card__without-picture')[0].style.display = 'block';
+
+		}
+	}
+
+	// End Check If There Is Broker Picture
+
+
+
+	// Start Check If There Is Feed Full In The Broker card
+
+	if(document.getElementsByClassName('profile-card').length > 0) {
+		let titleCardEllement = document.getElementsByClassName('title---JS');
+
+		for(let i = 0; i < titleCardEllement.length; i++) {
+			let childEllement = $(titleCardEllement[i]).siblings('.feed---JS');
+
+			if(!childEllement[0].innerHTML.length > 0) {
+				titleCardEllement[i].parentNode.remove();
+			}
+		}
+
+	}
+
+	// End Check If There Is Feed Full In The Broker card
+
+
+
+	// Start Fill Background In Start
+
+	if(document.getElementsByClassName('rating__progress').length > 0) {
+		let progress = document.querySelectorAll('.rating__progress');
+
+		for(let i = 0; i < progress.length; i++) {
+			let value = progress[i].getAttribute('data-percent') + "%";
+			progress[i].style.width = value;
+		}
+	}
+
+
+	// End Fill Background In Start
+
+
+
+	// Start Fill Color Gray If Custom Broker Percent is 0
+
+
+	if(document.getElementsByClassName('rating__custom-percent').length > 0) {
+		let customPercent = document.querySelector('.rating__custom-percent-number');
+		customPercent = customPercent.innerHTML.slice(0, -1);  // Remove the last char (%) in ellemnt
+
+		if(customPercent == '0') {
+			let ratingOptions = document.querySelector('.profile-info__options');
+			ratingOptions.classList.add('profile-info__options--no-active');
+		}
+	}
+
+	// End Fill Color Gray If Custom Broker Percent is 0
+
+
+
 	// Start Check Outside Clicks
 
 		$(".burger").click(function(e) {
@@ -94,40 +224,47 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	// End Check Click Btn And Scroll
 
-	var languageBtn = document.getElementById('language__btn');
+	if(document.getElementsByClassName('language__btn').length > 0) {
+		var languageBtn = document.getElementById('language__btn');
 
-	languageBtn.addEventListener('click', function(e) {
-		e.preventDefault();
-		$('.language__list').toggleClass('language__list--open');
-	});
+		languageBtn.addEventListener('click', function(e) {
+			e.preventDefault();
+			$('.language__list').toggleClass('language__list--open');
+		});
 
-	$('.language__checkbox').each(function() {
-		if($(this).is(":checked")) {
+		$('.language__checkbox').each(function() {
+			if($(this).is(":checked")) {
 
-			let langName = $(this).data('lang');
-			$('<i class="flag-icon"></i>').addClass(langName).attr('id', langName).hide().fadeIn('slow').appendTo(languageBtn);
-		} else {
-			let langName = $(this).data('lang');
+				let langName = $(this).data('lang');
+				$('<i class="flag-icon"></i>').addClass(langName).attr('id', langName).hide().fadeIn('slow').appendTo(languageBtn);
+			} else {
+				let langName = $(this).data('lang');
 
-			let elem = document.getElementById(langName);
-			console.log(elem);
-			$(elem).fadeOut('slow').remove();
-		}
-	})
+				let elem = document.getElementById(langName);
+				console.log(elem);
+				$(elem).fadeOut('slow').remove();
+			}
+		})
 
-	$('html').on('click', '.language__checkbox', function() {
-		if($(this).is(":checked")) {
+		$('html').on('click', '.language__checkbox', function() {
+			if($(this).is(":checked")) {
 
-			let langName = $(this).data('lang');
-			$('<i class="flag-icon"></i>').addClass(langName).attr('id', langName).appendTo(languageBtn);
-		} else {
-			let langName = $(this).data('lang');
+				let langName = $(this).data('lang');
+				$('<i class="flag-icon"></i>').addClass(langName).attr('id', langName).appendTo(languageBtn);
+			} else {
+				let langName = $(this).data('lang');
 
-			let elem = document.getElementById(langName);
-			console.log(elem);
-			$(elem).remove();
-		}
-	})
+				let elem = document.getElementById(langName);
+				console.log(elem);
+				$(elem).remove();
+			}
+		})
+	}
+
+
+
+
+
 
 	var windowWidth = window.innerWidth;
 
@@ -138,7 +275,7 @@ document.addEventListener('DOMContentLoaded', function() {
 			slidesToScroll: 1,
 			adaptiveHeight: true,
 			arrows: false,
-			autoplay: false,
+			autoplay: true,
 			autoplaySpeed: 5000,
 			centerMode: true,
 			focusOnSelect: false,
@@ -157,7 +294,5 @@ document.addEventListener('DOMContentLoaded', function() {
 				variableWidth: true
 				});
 	}
-
-
 
 })
