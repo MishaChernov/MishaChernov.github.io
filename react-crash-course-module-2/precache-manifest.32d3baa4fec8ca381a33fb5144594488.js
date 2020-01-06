d4b5d683c18d30b233542bfcdbdd8ce0f9adfb63
Misha Chernov <80amulet63@gmail.com>
2020-01-07 01:09:29 +0200

    Added a new launt react-crash-course-module-2

---
+++ react-crash-course-module-2/precache-manifest.32d3baa4fec8ca381a33fb5144594488.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ad5428c5126e451e15772335c50c7a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73b31778e17d34f5abf9",
+    "url": "/static/css/3.8066aea3.chunk.css"
+  },
+  {
+    "revision": "57c975f175d46afcce02",
+    "url": "/static/css/main.0211f16a.chunk.css"
+  },
+  {
+    "revision": "1020e98599a8fcaef601",
+    "url": "/static/js/2.5dc8ca7e.chunk.js"
+  },
+  {
+    "revision": "73b31778e17d34f5abf9",
+    "url": "/static/js/3.0d26f49c.chunk.js"
+  },
+  {
+    "revision": "57c975f175d46afcce02",
+    "url": "/static/js/main.a535dd81.chunk.js"
+  },
+  {
+    "revision": "989eecbf1ecdd8c07213",
+    "url": "/static/js/runtime-main.c17ec98f.js"
+  },
+  {
+    "revision": "5922c1d0ef4b11375dc4d732a765ab62",
+    "url": "/static/media/Error.5922c1d0.svg"
+  }
+]);
